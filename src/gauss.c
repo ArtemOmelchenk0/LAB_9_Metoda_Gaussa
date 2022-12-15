b579023ad413bb00c5b85ad97e316dde12fc7fa8
awkwarrd <heregoesnothingowo@gmail.com>
2022-12-15 15:46:05 +0100

    gauss v3

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "gauss.h"
+#include <math.h>
+#include "mat_io.h"
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate( Matrix *mat, Matrix *b){
+
+int eliminate( Matrix *mat, Matrix *b) {
     int k, j, n,i;
     double d;
     n=mat->r;
-    for(k = 0; k < n; k++){
+    for (k = 0; k < n - 1; k++) {
+        int kmax = k;
+        for (j = k + 1; j < n; j++)
+            if ( fabs (mat->data[j][k]) > fabs(mat->data[kmax][k]) )
+                kmax = j;
+        if (kmax != k) {
+            double *tmp = mat->data[k];
+            mat->data[k] = mat->data[kmax];
+            mat->data[kmax] = tmp;
+            double *btmp = b->data[k];
+            b->data[k] = b->data[kmax];
+            b->data[kmax] = btmp;
+        }
         for(j = k + 1; j < n; j++){
             if(mat->data[k][k] == 0){
                 return 1;
